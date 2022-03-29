306646f25fb24df4dd105613c0dddd5fe37ac1bd
Yuda Kaito <211019@st.yoshida-g.ac.jp>
2022-03-29 16:46:04 +0900

    リザルトでスコア表示

--- result.cpp
+++ result.cpp
@@ -17,7 +17,9 @@
 #include "light.h"
 #include "mode.h"
 #include "utility.h"
-
+#include "number.h"
+#include "score.h"
+#include "ranking.h"
 #include "input.h"
 
 //=============================================================================
@@ -34,6 +36,7 @@ const int	MAX_TIME = 600;	// タイムの最大値
 namespace
 {
 int	s_nIdxBG;			// 背景の矩形のインデックス
+int	s_nIdxUI[2];			// UIの矩形のインデックス
 int	s_nGaugeIdxGray;	// ゲージのインデックスの保管
 int	s_nGaugeIdxYellow;	// ゲージのインデックスの保管
 int	s_nTime;			// タイム
@@ -63,6 +66,7 @@ void InitResult(void)
 	
 	// ゲージの設定(黄色)
 	s_nGaugeIdxYellow = SetGauge(D3DXVECTOR3(0.0f, SCREEN_HEIGHT * 0.5f, 0.0f), GetColor(COLOR_YELLOW), (GetLight() - 1) * (SCREEN_WIDTH / 16.0f), SCREEN_HEIGHT, GAUGE_LEFT);
+
 	// ゲージの色の設定(黄色)
 	SetColorGauge(s_nGaugeIdxYellow, D3DXCOLOR(GetColor(COLOR_YELLOW).r, GetColor(COLOR_YELLOW).g, GetColor(COLOR_YELLOW).b, s_fGaugeAlpha));
 	
@@ -79,6 +83,43 @@ void InitResult(void)
 		// 矩形の色の設定
 		SetColorRectangle(s_nIdxBG, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
 	}
+
+	{// 今回のスコア
+	 // 矩形の設定
+		s_nIdxUI[0] = SetRectangle(TEXTURE_YourScore);
+
+		D3DXVECTOR3 pos = D3DXVECTOR3(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.25f, 0.0f);
+		D3DXVECTOR3 size = D3DXVECTOR3(600.0f, 100.0f, 0.0f);
+
+		// 矩形の位置の設定
+		SetPosRectangle(s_nIdxUI[0], pos, size);
+
+		// 矩形の色の設定
+		SetColorRectangle(s_nIdxUI[0], D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
+
+		pos = D3DXVECTOR3(SCREEN_WIDTH * 0.65f, SCREEN_HEIGHT * 0.25f, 0.0f);
+		size = D3DXVECTOR3(70.0f, 90.0f, 0.0f);
+		SetNumber(pos,size,GetColor(COLOR_WHITE),GetLight() - 1,1,false);
+	}
+
+	{// ベストスコア
+	 // 矩形の設定
+		s_nIdxUI[1] = SetRectangle(TEXTURE_BestScore);
+
+		D3DXVECTOR3 pos = D3DXVECTOR3(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 0.0f);
+		D3DXVECTOR3 size = D3DXVECTOR3(600.0f, 100.0f, 0.0f);
+
+		// 矩形の位置の設定
+		SetPosRectangle(s_nIdxUI[1], pos, size);
+
+		// 矩形の色の設定
+		SetColorRectangle(s_nIdxUI[1], D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
+
+		pos = D3DXVECTOR3(SCREEN_WIDTH * 0.65f, SCREEN_HEIGHT * 0.5f, 0.0f);
+		size = D3DXVECTOR3(70.0f, 90.0f, 0.0f);
+		SetNumber(pos, size, GetColor(COLOR_WHITE), GetRanking(), 1, false);
+	}
+
 }
 
 //=============================================================================
